f39231588c05b6b414e4abb649066ecd6c868272
Rcaradec <remi.caradec@matou-technologies.fr>
2024-09-02 16:12:53 +0200

    components refacto

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,60 @@
+import React, { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
+import { tasksListBase, categories } from "./data/tasksListData.ts";
+import { Task } from "./components/Form.tsx";
+import List from "./components/List.tsx";
 
-function App() {
+const App: React.FC = () => {
+  const [tasksList, setTasksList] = useState<Task[]>(tasksListBase);
+  const [filteredTasksList, setFilteredTasksList] = useState<Task[]>(tasksList);
+  const [selectedCat, setSelectedCat] = useState<string>("");
+
+  const handleDelete = (taskToDelete: Task) => {
+    const updatedTasksList = tasksList.filter(
+      (task) => task.description !== taskToDelete.description
+    );
+    setTasksList(updatedTasksList);
+    setFilteredTasksList(updatedTasksList);
+  };
+
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedCategory = event.target.value;
+    setSelectedCat(selectedCategory);
+    if (selectedCategory === "") {
+      setFilteredTasksList(tasksList);
+    } else {
+      setFilteredTasksList(
+        tasksList.filter((task) => task.category === selectedCategory)
+      );
+    }
+  };
+
+  const handleResetClick = () => {
+    setFilteredTasksList(tasksList);
+    setSelectedCat("");
+  };
+
   return (
     <>
-      <h1 className="text-primary mb-5">Gestionnaire de dépenses</h1>
-      <Form />
+      <h1 className="text-primary">Gestionnaire de dépenses</h1>
+      <Form
+        tasksList={tasksList}
+        setTasksList={setTasksList}
+        setFilteredTasksList={setFilteredTasksList}
+        selectedCat={selectedCat}
+        setSelectedCat={setSelectedCat}
+        categories={categories}
+      />
+      <List
+        filteredTasksList={filteredTasksList}
+        handleDelete={handleDelete}
+        handleSelectChange={handleSelectChange}
+        handleResetClick={handleResetClick}
+        selectedCat={selectedCat}
+      />
     </>
   );
-}
+};
 
 export default App;
-
-// J'ai besoin de:
-// - un composant Form
-// - un composant Select Categories
-// - un composant List avec un Select de filtre
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,41 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-
-const categories: string[] = ["Courses", "Utilitaire", "Loisirs"];
-const tasksListBase = [
-  {
-    description: "Lait",
-    amount: 5,
-    category: "Courses",
-  },
-  {
-    description: "Vaisselle",
-    amount: 8,
-    category: "Utilitaire",
-  },
-  {
-    description: "Repassage",
-    amount: 10,
-    category: "Utilitaire",
-  },
-  {
-    description: "Pétanque",
-    amount: 15,
-    category: "Loisirs",
-  },
-  {
-    description: "Ping-pong",
-    amount: 35,
-    category: "Loisirs",
-  },
-  {
-    description: "Paic citron",
-    amount: 3,
-    category: "Courses",
-  },
-];
 
 const schema = z.object({
   description: z
@@ -45,13 +11,24 @@ const schema = z.object({
   category: z.string().min(1, { message: "Veuillez sélectionner une option" }),
 });
 
-type Task = z.infer<typeof schema>;
+export type Task = z.infer<typeof schema>;
 
-const Form = () => {
-  const [tasksList, setTasksList] = useState<Task[]>(tasksListBase);
-  const [filteredTasksList, setFilteredTasksList] = useState<Task[]>(tasksList);
-  const [selectedCat, setSelectedCat] = useState<string>("");
+type Props = {
+  tasksList: Task[];
+  setTasksList: React.Dispatch<React.SetStateAction<Task[]>>;
+  setFilteredTasksList: React.Dispatch<React.SetStateAction<Task[]>>;
+  selectedCat: string;
+  setSelectedCat: React.Dispatch<React.SetStateAction<string>>;
+  categories: string[];
+};
 
+const Form = ({
+  tasksList,
+  setTasksList,
+  setFilteredTasksList,
+
+  categories,
+}: Props) => {
   const {
     handleSubmit,
     register,
@@ -63,31 +40,6 @@ const Form = () => {
   const onSubmit = (newTask: Task) => {
     setTasksList([...tasksList, newTask]);
     setFilteredTasksList([...tasksList, newTask]);
-  };
-
-  const handleDelete = (taskToDelete: Task) => {
-    const updatedTasksList = tasksList.filter(
-      (task) => task.description !== taskToDelete.description
-    );
-    setTasksList(updatedTasksList);
-    setFilteredTasksList(updatedTasksList);
-  };
-
-  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const selectedCategory = event.target.value;
-    setSelectedCat(selectedCategory);
-    if (selectedCategory === "") {
-      setFilteredTasksList(tasksList);
-    } else {
-      setFilteredTasksList(
-        tasksList.filter((task) => task.category === selectedCategory)
-      );
-    }
-  };
-
-  const handleResetClick = () => {
-    setFilteredTasksList(tasksList);
-    setSelectedCat("");
   };
 
   return (
@@ -139,63 +91,8 @@ const Form = () => {
         {errors.category && (
           <p className="text-danger">{errors.category.message} </p>
         )}
-        <button className="btn btn-primary">Submit</button>
+        <button className="btn btn-primary">Envoyer</button>
       </form>
-
-      <select
-        className="form-select mb-3"
-        id="category"
-        aria-label="Categories Select"
-        onChange={handleSelectChange}
-      >
-        <option value="">Filtrer par catégorie</option>
-        {categories.map((category) => (
-          <option key={category} value={category}>
-            {category}
-          </option>
-        ))}
-      </select>
-      <div className="d-flex gap-2 align-items-center">
-        <p className="mb-0">Réintialiser le filtre de catégories:</p>
-        <button
-          className="btn border border-primary btn-close"
-          onClick={handleResetClick}
-        ></button>
-      </div>
-      {selectedCat !== "" && (
-        <p className="">
-          La liste affiche la catégorie:{" "}
-          <strong className="text-primary">{selectedCat}</strong>
-        </p>
-      )}
-
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Description</th>
-            <th scope="col">Montant</th>
-            <th scope="col">Catégorie</th>
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredTasksList.map((task) => (
-            <tr key={task.description}>
-              <td>{task.description}</td>
-              <td>{task.amount}€</td>
-              <td>{task.category}</td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(task)}
-                >
-                  Supprimer
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </>
   );
 };
